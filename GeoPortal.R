2825751464219c50fd9c689483ab8420649c6dba
Nixon Rolando Garcia Ramirez <125457406+NixonGarciaRamirez@users.noreply.github.com>
2024-09-08 21:11:39 -0500

    Primer modelo de interfaz

--- GeoPortal.R
+++ GeoPortal.R
@@ -1,3 +1,8 @@
 # Instalasion de librerias
 
 install.packages(c("shiny", "leaflet", "sf", "raster", "tmap", "mapview"))
+
+ui <- fluidPage(
+   titlePanel("Geoportal Básico en R"),
+   leafletOutput("mymap")
+)
