4e7e25002d7e7944f3f4261afac8facc8b5bbaa2
Nixon Rolando Garcia Ramirez <125457406+NixonGarciaRamirez@users.noreply.github.com>
2024-09-18 17:54:47 -0500

    Ver a bogota

---
+++ GeoBogota.R
@@ -0,0 +1,14 @@
+ui <- fluidPage(
+  titlePanel("Vizualizadno a Bogota"),
+  leafletOutput("mymap")
+)
+
+server <- function(input, output) {
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      addMarkers(lng = -74.0721, lat = 4.7110, popup = "Bogotá, Colombia")
+  })
+}
+
+shinyApp(ui = ui, server = server
