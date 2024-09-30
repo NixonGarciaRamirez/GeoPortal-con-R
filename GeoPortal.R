a5bd1ee843d667a121c4abc32b5fe2a6e4b50b1d
Nixon Rolando Garcia Ramirez <125457406+NixonGarciaRamirez@users.noreply.github.com>
2024-09-30 15:20:46 -0500

    GeoPOrtar Capa por defecto V1

--- GeoPortal.R
+++ GeoPortal.R
@@ -1,39 +1,118 @@
+####################### Interfaz grafica ###############################################
+
+
 # Define la ruta de tu archivo shapefile (incluyendo todos los archivos asociados)
 shapefile_path <- "D:/NIXON/MI MUNDO PROPIO/08 SIG/R CON GEE/GeoPortal/SHP/CORREDORES2.shp"
 
-# Definir la interfaz de usuario
+# Definir la interfaz de usuario (UI)
 ui <- fluidPage(
-  titlePanel("GeoPortal"),
-  leafletOutput("mapa") # Salida del mapa
+  # Título del GeoPortal
+  titlePanel("GeoPortal con Mapas Base y Capas Shapefile/Raster para el proyecto de Corredores Biologicos"),
+  
+  # Diseño con panel lateral y panel principal
+  sidebarLayout(
+    sidebarPanel(
+      h3("Opciones de Visualización"),
+      
+      # Selección del mapa base
+      radioButtons("map_type", "Selecciona el tipo de mapa base:",
+                   choices = c("OpenStreetMap" = "OSM",
+                               "CartoDB Positron" = "CartoDB",
+                               "Stamen Toner" = "StamenToner",
+                               "Stamen Terrain" = "StamenTerrain",
+                               "Esri World Imagery" = "Esri")),
+      
+      # Botón para cargar shapefile
+      fileInput("shapefile", "Subir un Shapefile", 
+                multiple = TRUE, 
+                accept = c(".shp", ".dbf", ".shx", ".prj")),
+      
+      # Botón para cargar raster
+      fileInput("rasterfile", "Subir un Raster (.tif)", 
+                multiple = FALSE, 
+                accept = c(".tif"))
+    ),
+    
+    mainPanel(
+      leafletOutput("mapa", height = "600px")
+    )
+  )
 )
 
-# Definir el servidor
+
+################## Servidores  ################################
+
+# Definir el servidor (Server)
 server <- function(input, output, session) {
   
-  # Cargar el shapefile directamente desde el código
+  # Función para cargar shapefile
   cargar_shapefile <- reactive({
     shape <- st_read(shapefile_path)  # Cargar shapefile desde la ruta
     return(shape)
   })
   
-  # Crear el mapa base
-  output$mapa <- renderLeaflet({
-    leaflet() %>%
-      addTiles() %>%
-      setView(lng = -75.45738, lat =6.64972, zoom = 12)
+  # Función para cargar raster
+  cargar_raster <- reactive({
+    req(input$rasterfile)
+    
+    # Obtener ruta del archivo raster
+    raster_path <- input$rasterfile$datapath
+    
+    # Cargar el archivo raster usando la librería raster
+    ras <- raster(raster_path)
+    
+    return(ras)
   })
   
-  # Añadir el shapefile al mapa
+  # Renderizar el mapa base en Leaflet
+  output$mapa <- renderLeaflet({
+    leaflet() %>%
+      setView(lng = -75.45738, lat = 6.64972, zoom = 10)  # Centrado en Santa Rosa de Osos, Colombia
+  })
+  
+  # Actualizar el mapa base dinámicamente
+  observe({
+    proxy <- leafletProxy("mapa")
+    
+    # Remover cualquier capa previa
+    proxy %>% clearTiles()
+    
+    # Añadir la capa de mapa seleccionada
+    if (input$map_type == "OSM") {
+      proxy %>% addTiles()  # OpenStreetMap
+    } else if (input$map_type == "CartoDB") {
+      proxy %>% addProviderTiles(providers$CartoDB.Positron)
+    } else if (input$map_type == "StamenToner") {
+      proxy %>% addProviderTiles(providers$Stamen.Toner)
+    } else if (input$map_type == "StamenTerrain") {
+      proxy %>% addProviderTiles(providers$Stamen.Terrain)
+    } else if (input$map_type == "Esri") {
+      proxy %>% addProviderTiles(providers$Esri.WorldImagery)
+    }
+  })
+  
+  # Añadir shapefile al mapa
   observe({
     shape <- cargar_shapefile()
-    req(shape)  # Asegurarse de que el shapefile está cargado
+    req(shape)
     
     leafletProxy("mapa") %>%
-      clearShapes() %>%
+      clearShapes() %>%  # Limpiar formas anteriores
       addPolylines(data = shape, color = "blue", weight = 2, fillOpacity = 0.5)
+  })
+  
+  # Añadir raster al mapa
+  observe({
+    ras <- cargar_raster()
+    req(ras)
+    
+    leafletProxy("mapa") %>%
+      clearImages() %>%  # Limpiar imágenes anteriores
+      addRasterImage(ras, opacity = 0.7)
   })
 }
 
-# Ejecutar la aplicación
-shinyApp(ui, server)
+############### Ejecucion #############################
 
+# Ejecutar la aplicación Shiny
+shinyApp(ui = ui, server = server)
