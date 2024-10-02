b1515e949679f2af1e2d144690148e7eb23c3656
Nixon Rolando Garcia Ramirez <125457406+NixonGarciaRamirez@users.noreply.github.com>
2024-10-01 21:06:29 -0500

    Parte 2

--- GeoPortal2.R
+++ GeoPortal2.R
@@ -2,8 +2,8 @@ library(shiny)
 library(leaflet)
 
 # Cargar el shapefile
-shp_path <- "D:/NIXON/MI MUNDO PROPIO/08 SIG/R CON GEE/GeoPortal/SHP/CORREDORES2.shp"
-shp_data <- st_read(shp_path)
+shp_path <- "D:/NIXON/MI MUNDO PROPIO/08 SIG/SHAPES UTILES PARA ANALISIS/MAPAS DE UBICACION/VALLE DE ABURRA/Valle de aburra.shp"
+shp_data <- st_read(shp_path, quiet = T)
 
 # Obtener los nombres de los atributos del shapefile
 attribute_names <- names(shp_data)
@@ -41,7 +41,7 @@ server <- function(input, output, session) {
   #Filtro
   filtered_data <- reactive({
     req(input$attribute, input$value)
-    shp_data[shp_data[[input$attribute]] == input$value, ]
+    shp_data %>% filter(!!sym(input$attribute) == input$value)
   })
   
   #Mapa
@@ -50,11 +50,11 @@ server <- function(input, output, session) {
       addTiles() %>%
       addProviderTiles(providers$Esri.WorldImagery)%>%
       setView(lng = -75.45738, lat = 6.64972, zoom = 10) %>%
-      addPolylines(data = shp_data, color = "blue", weight = 2, opacity = 1.0, fillOpacity = 0.5)
+      addPolygons(data = shp_data, color = "blue", weight = 2, opacity = 1.0, fillOpacity = 0.5)
   })
   
   output$info <- renderPrint({
-    paste("Filtros aplicados:", paste(input$filter, collapse = ", "))
+    paste("Atributo seleccionado:", input$attribute, "\nValor del atributo:", input$value)
   })
   
 }
